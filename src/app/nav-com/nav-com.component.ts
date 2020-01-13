52e83cec2e14f6c9305bca37a20ca4bd5e5cafbb
Khloud355 <khloudrammah69@gmail.com>
2020-01-13 11:50:53 +0200

    first commit

---
+++ src/app/nav-com/nav-com.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nav-com',
+  templateUrl: './nav-com.component.html',
+  styleUrls: ['./nav-com.component.css']
+})
+export class NavComComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
